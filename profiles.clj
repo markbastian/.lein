9f0f7af798c5c901161cc0a2c7619c7033500fd5
Mark Bastian <markbastian@gmail.com>
2016-05-01 23:32:02 -0600

    Added profile entries.

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [lein-ancient "0.6.8"]]}} 
+                  [lein-ancient "0.6.8"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-simpleton "1.3.0"]
+                  [lein-kibit "0.1.2"]]
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}} 
